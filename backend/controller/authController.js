f939337a2fa98bd6d2efba25bbeaf3d0a75e28ad
PRITHVI RAJ PANDEY <infodev430@gmail.com>
2026-05-21 20:13:35 +0545

    fix gmail ipv6 issue

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -119,7 +119,10 @@ const forgotPassword = async (req, res) => {
     // Gmail configuration
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
